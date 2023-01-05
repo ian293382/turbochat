9b9e3286b2bad5f8494349882f9d88cb249096ee
YuYuan <moshou5571228@gmail.com>
2023-01-05 17:40:43 +0800

    debug setup turbo_devise_controller

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController
+class TurboDeviseController < ApplicationController
   class Responder < ActionController::Responder
     def to_turbo_stream
       controller.render(options.merge(formats: :html))
@@ -8,7 +8,7 @@ class ApplicationController < ActionController
       elsif has_errors? && default_action
         render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
-        redirect_to navigation_loction
+        redirect_to navigation_location
       end
     end
   end
