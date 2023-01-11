13df13b801a16ff0c95873fa9b273fa66f6c1cdf
YuYuan <moshou5571228@gmail.com>
2023-01-12 02:24:38 +0800

    fix type model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  scope :all_except, -> (user) { where.not(id: user)}
-  after_create_commit { broadcast_append_to "users"}
+
+  validates_uniqueness_of :username
+  scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to "users" }
 end
