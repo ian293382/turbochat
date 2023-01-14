b5c9a857c1c3f83e4e5e67efb83e75e3f7be027d
YuYuan <moshou5571228@gmail.com>
2023-01-14 18:41:56 +0800

    add single_room show action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,12 +4,22 @@ class RoomsController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
 
-    @single_room = Room.find(params[:id])
-
     @users = User.all_except(current_user)
   end
 
   def create
     @room = Room.create(name: params['room']['name'])
   end
+
+  def show
+    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(current_user)
+    @room = Room.new
+    @message = Message.new
+
+    @messages = @single_room.messages.order(created_at: :desc)
+    render 'index'
+  end
+
 end
