dc4b2a4118d8d071ed690dadafe47b9c4dbea9ae
YuYuan <moshou5571228@gmail.com>
2023-01-15 02:01:36 +0800

    Identify private rooms and implement to user join in to room action

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,7 @@ class Message < ApplicationRecord
   after_create_commit { broadcast_append_to room }
   before_create :confirm_participant
 
+  # from Building a Real-Time Chat App in Rails Using Action Cable and Turbo
   def confirm_participant
     if room.is_private
       is_participant = Participant.where(user_id: user.id, room_id: room.id).first
