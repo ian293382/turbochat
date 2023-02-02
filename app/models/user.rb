abec3319c24f2513989215f4b00cedd46b617ced
YuYuan <moshou5571228@gmail.com>
2023-02-02 17:06:55 +0800

    Add user profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_uniqueness_of :username
   scope :all_except, ->(user) { where.not(id: user) }
-  after_create_commit { broadcast_append_to "users" }
+  after_create_commit { broadcast_append_to 'users' }
   has_many :messages, dependent: :destroy
+  has_one_attached :pfp
+
+  after_commit :add_default_pfp, on: %i[create update]
+
+  def pfp_thumbnail
+    pfp.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  private
+
+  def add_default_pfp
+    return if pfp.attached?
+
+    pfp.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.jpg')),
+      filename: 'default_profile.jpg',
+      content_type: 'image/png'
+    )
+  end
 end
