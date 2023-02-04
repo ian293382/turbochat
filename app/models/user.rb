d820f1f5b93e0b650154b271da13bf8a67d24fd0
YuYuan <moshou5571228@gmail.com>
2023-02-04 23:06:54 +0800

    fix chat_pfp and add status method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,39 @@ class User < ApplicationRecord
 
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
+  # set status
+  after_update_commit { broadcast_update }
+
   has_many :messages, dependent: :destroy
   has_one_attached :pfp
+
+  enum status: %i[offline away online]
 
   after_commit :add_default_pfp, on: %i[create update]
 
   def pfp_thumbnail
     pfp.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  def chat_pfp
+    pfp.variant(resize_to_limit: [50,50]).processed
+  end
+
+  def broadcast_update
+    broadcast_replace_to 'user_status', partial: 'user/status', user: self
+  end
+
+  def status_to_css
+    case status
+    when 'online'
+      'bg-success'
+    when 'away'
+      'bg-warning'
+    when 'offline'
+      'bg-danger'
+    else
+      'bg-dark'
+    end
   end
 
   private
