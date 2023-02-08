a3f166a517908b0e5203e4ca3b97239f544cbefa
YuYuan <moshou5571228@gmail.com>
2023-02-08 17:21:29 +0800

    fix javascript scroll and implement attachments install icon cdn

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,19 @@ class Message < ApplicationRecord
   after_create_commit { broadcast_append_to room }
   before_create :confirm_participant
 
+  has_many_attached :attachments, dependent: :destroy
+
+  def chat_attachment(index)
+    target = attachments[index]
+    return unless attachments.attached?
+
+    if target.image?
+      target.variant(resize_to_limit: [150, 150]).processed
+    elsif target.video?
+      target.variant(resize_to_limit: [150,150]).processed
+    end
+  end
+
   # from Building a Real-Time Chat App in Rails Using Action Cable and Turbo
   def confirm_participant
     if room.is_private
